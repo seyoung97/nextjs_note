e5e433bea534bc97d9146b3880930ffa2d366adc
seyoung97 <seyoung1st@gmail.com>
2023-07-19 16:36:53 +0900

    Not found page

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 type Props = {
   params: {
     slug: string;
@@ -5,9 +7,13 @@ type Props = {
 };
 
 export default function PantsPage({ params }: Props) {
+  if (params.slug == "nothing") {
+    notFound();
+  }
   return <h1>{params.slug} 소개 페이지!</h1>;
 }
 
+// 페이지 미리 생성하는 함수
 export function generateStaticParams() {
   const products = ["pants", "skirt"];
   return products.map((product) => ({
