482ebb3b147409fc3824a75f44ea897a9c7db05f
seyoung97 <seyoung1st@gmail.com>
2023-07-19 18:55:34 +0900

    링크 만들기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import styles from "./layout.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +22,9 @@ export default function RootLayout({
         <header className={styles.header}>
           <h1>Demo Note</h1>
           <nav className={styles.nav}>
-            <a href="">About</a>
-            <a href="">Contact</a>
+            <Link href="/products">Products</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
           </nav>
         </header>
         {children}
--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import styles from "./layout.module.css";
 
 export default function RootLayout({
@@ -8,8 +10,8 @@ export default function RootLayout({
   return (
     <>
       <nav className={styles.nav}>
-        <a href="">여성옷</a>
-        <a href="">남성옷</a>
+        <Link href="/products/women">여성옷</Link>
+        <a href="/products/man">남성옷</a>
       </nav>
       <section className={styles.product}>{children}</section>
     </>
